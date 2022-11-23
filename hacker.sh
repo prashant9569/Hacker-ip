51784b09f4f8a57d81153f96537298e0954731d0
Prashant <114965028+prashant9569@users.noreply.github.com>
2022-11-23 11:22:48 +0000

    Add files via upload

---
+++ hacker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+ifconfig eth0 81.255.13.197
+sleep 30.5
+ifconfig eth0 217.97.101.134
+sleep 30.5
+ifconfig eth0 82.200.215.136
+sleep 30.5
+ifconfig eth0 14.99.85.141
+sleep 30.5
+ifconfig eth0 86.57.219.210
+sleep 30.5
+ifconfig eth0 47.89.153.213
+sleep 30.5
+ifconfig eth0 168.119.103.160
+sleep 30.5
+ifconfig eth0 167.172.171.151
+sleep 30.5
+ifconfig eth0 132.148.85.91
+sleep 30.5
+ifconfig eth0 132.148.85.91
+sleep 30.5
+ifconfig eth0 149.56.86.231
+sleep 30.5
+ifconfig eth0 186.46.168.45
+sleep 30.5
+ifconfig eth0 43.250.127.98
+sleep 30.5
+ifconfig eth0 206.189.37.192
+sleep 30.5
+ifconfig eth0 139.59.51.139
+sleep 30.5
+ifconfig eth0 157.230.103.189
+
+
+
